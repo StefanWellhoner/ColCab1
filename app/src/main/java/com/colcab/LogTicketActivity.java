5700dfaf4da945023d2b5ca3e304c8e726072a69
Murray O'Brien <61970823+murrayobrienIT@users.noreply.github.com>
2020-06-15 11:09:25 +0200

    murrayLogActivity
    New log ticket screen.

---
+++ app/src/main/java/com/colcab/LogTicketActivity.java
@@ -0,0 +1,14 @@
+package com.colcab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_ticket);
+    }
+}
