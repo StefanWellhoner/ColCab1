137c02b1ed9783180bed5f2a8030622caa3d54c3
StefanWellhoner <stefanwellhoner@ymail.com>
2020-06-18 12:20:28 +0200

    + Updated Home Screen + Added Hamburger bar to toolbar

--- app/src/main/java/com/colcab/HomePage.java
+++ app/src/main/java/com/colcab/HomePage.java
@@ -1,19 +1,59 @@
 package com.colcab;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
-public class HomePage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public void onClosedTicket(View view) {
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        //@TODO insert intents for navigation
+        switch (id){
+            case R.id.nav_home:
+                break;
+            case R.id.nav_log_ticket:
+                break;
+            case R.id.nav_view_closed:
+                break;
+            case R.id.nav_view_schedule:
+                break;
+            case R.id.nav_exit:
+                System.exit(0);
+                break;
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
