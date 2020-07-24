cf741846c0ae62b923c422fd32deb576d36e103b
Stefan Wellhoner <stefanwellhoner@ymail.com>
2020-07-24 17:20:53 +0200

    ewvw

--- app/src/main/java/com/colcab/OpenTicketsFragment.java
+++ app/src/main/java/com/colcab/OpenTicketsFragment.java
@@ -2,7 +2,12 @@ package com.colcab;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,5 +22,50 @@ public class OpenTicketsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_open_tickets, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        RecipientPagerAdapter recipientAdapter = new RecipientPagerAdapter(getChildFragmentManager());
+        ViewPager viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(recipientAdapter);
+    }
+
+}
+class RecipientPagerAdapter extends FragmentStatePagerAdapter {
+
+    public RecipientPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new ScheduledTicketsFragment();
+            case 1:
+                return new UnscheduledTicketsFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Scheduled";
+            case 1:
+                return "Unscheduled";
+            default:
+                return "";
+        }
     }
 }
