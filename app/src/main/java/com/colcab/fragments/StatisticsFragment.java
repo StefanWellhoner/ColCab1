a912fa4bf021e396d673f0f962f00d2450dbe0e8
Chris2021998 <56068706+Chris2021998@users.noreply.github.com>
2020-10-11 14:01:35 +0200

    Admin info page update

--- app/src/main/java/com/colcab/fragments/StatisticsFragment.java
+++ app/src/main/java/com/colcab/fragments/StatisticsFragment.java
@@ -17,8 +17,17 @@ import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Pie;
 import com.colcab.R;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LimitLine;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -62,13 +71,12 @@ public class StatisticsFragment extends Fragment {
 //        AnyChartView anyChartView = (AnyChartView) view.findViewById(R.id.any_chart_view);
 //        anyChartView.setChart(pie);
 
-        List<PieEntry> entry1 = new ArrayList<PieEntry>();
-        List<PieEntry> entry2 = new ArrayList<PieEntry>();
+        List<PieEntry> pieEntry = new ArrayList<PieEntry>();
 
-        entry1.add(new PieEntry(5, "Scheduled"));
-        entry1.add(new PieEntry(3, "Unscheduled"));
+        pieEntry.add(new PieEntry(5, "Scheduled"));
+        pieEntry.add(new PieEntry(3, "Unscheduled"));
 
-        PieDataSet dataSet = new PieDataSet(entry1, "Tickets");
+        PieDataSet dataSet = new PieDataSet(pieEntry, "Tickets");
 
 //        LineDataSet dataSet = new LineDataSet(entries, "Names");
 
@@ -83,6 +91,59 @@ public class StatisticsFragment extends Fragment {
         PieChart chart = (PieChart) view.findViewById(R.id.chart);
         chart.setData(pieData);
         chart.invalidate();
+
+        List<BarEntry> barEntry = new ArrayList<BarEntry>();
+
+        barEntry.add(new BarEntry(0f, 30f));
+        barEntry.add(new BarEntry(1f, 80f));
+        barEntry.add(new BarEntry(2f, 60f));
+        barEntry.add(new BarEntry(3f, 50f));
+        barEntry.add(new BarEntry(4f, 70f));
+        barEntry.add(new BarEntry(5f, 60f));
+
+        BarDataSet barSet = new BarDataSet(barEntry, "Fault Graph");
+        barSet.setColor(Color.rgb(155, 155, 155));
+        barSet.setValueTextColor(Color.rgb(155,155,155));
+
+        BarData barData = new BarData();
+
+        barData.addDataSet(barSet);
+
+        BarChart barChart = (BarChart) view.findViewById(R.id.chart1);
+
+        Description desc = barChart.getDescription();
+        desc.setText("");
+
+        barChart.setData(barData);
+        barChart.invalidate();
+
+//        LineChart lineChart = (LineChart) view.findViewById(R.id.lineChart);
+//        lineChart.setTouchEnabled(true);
+//        lineChart.setPinchZoom(true);
+//
+//        LimitLine ll1 = new LimitLine(30f, "Title");
+//
+//        ll1.setLineColor(ResourcesCompat.getColor(getResources(), R.color.colorPrimary, null));
+//        ll1.setLineWidth(4f);
+//        ll1.enableDashedLine(10f, 10f, 0f);
+//
+//        ll1.setLabelPosition(LimitLine.LimitLabelPosition.RIGHT_BOTTOM);
+//        ll1.setTextSize(10f);
+//
+//        LimitLine ll2 = new LimitLine(35f, "");
+//        ll2.setLineWidth(4f);
+//        ll2.enableDashedLine(10f, 10f, 0f);
+//
+//        XAxis xAxis = lineChart.getXAxis();
+//        YAxis leftAxis = lineChart.getAxisLeft();
+//
+//        XAxis.XAxisPosition position = XAxis.XAxisPosition.BOTTOM;
+//        xAxis.setPosition(position);
+//
+//        lineChart.getDescription().setEnabled(true);
+//        Description description = new Description();
+//        description.setText("Week");
+//        description.setTextSize(15f);
 
         return view;
     }
